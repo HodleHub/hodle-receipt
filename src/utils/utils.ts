778f8f41fafdd6fd1fd6629b4db92c8a11ee4a8c
Thiago-Mota-Santos <lucimara1422@gmail.com>
2025-04-21 15:57:35 -0300

    fix(types) fix

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,45 +1,50 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 export function copyToClipboard(value: string): Promise<boolean> {
   if (navigator.clipboard && window.isSecureContext) {
-    return navigator.clipboard.writeText(value)
+    return navigator.clipboard
+      .writeText(value)
       .then(() => true)
-      .catch(() => false);
+      .catch(() => false)
   } else {
-    let textArea = document.createElement("textarea");
-    textArea.value = value;
-    
-    textArea.style.position = "fixed";
-    textArea.style.left = "-999999px";
-    textArea.style.top = "-999999px";
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
+    const textArea = document.createElement('textarea')
+    textArea.value = value
+
+    textArea.style.position = 'fixed'
+    textArea.style.left = '-999999px'
+    textArea.style.top = '-999999px'
+    document.body.appendChild(textArea)
+    textArea.focus()
+    textArea.select()
 
     return new Promise<boolean>((resolve) => {
-      const successful = document.execCommand('copy');
-      textArea.remove();
-      resolve(successful);
-    });
+      const successful = document.execCommand('copy')
+      textArea.remove()
+      resolve(successful)
+    })
   }
 }
 
-export function getURLParams(): { invoice: string | null, preimage: string | null } {
+export function getURLParams(): {
+  invoice: string | null
+  preimage: string | null
+} {
   if (typeof window === 'undefined') {
-    return { invoice: null, preimage: null };
+    return { invoice: null, preimage: null }
   }
-  
-  const url = new URL(window.location.href);
-  const invoice = url.searchParams.get('invoice');
-  const preimage = url.searchParams.get('proof') || url.searchParams.get('preimage');
+
+  const url = new URL(window.location.href)
+  const invoice = url.searchParams.get('invoice')
+  const preimage =
+    url.searchParams.get('proof') || url.searchParams.get('preimage')
 
   return {
     invoice: invoice ? invoice.toLowerCase() : null,
     preimage: preimage ? preimage.toLowerCase() : null,
-  };
-} 
+  }
+}
